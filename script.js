17828fb61fe04289712130df67f69a9e7538c5d4
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-06-19 11:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.querySelectorAll('strong');
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.add('bold-highlight');
+  }
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+  const boldWords = document.querySelectorAll('strong');
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.remove('bold-highlight');
+  }
 }
+
+const link = document.getElementById('highlightLink');
+link.addEventListener('mouseover', highlight);
+link.addEventListener('mouseout', return_normal);
